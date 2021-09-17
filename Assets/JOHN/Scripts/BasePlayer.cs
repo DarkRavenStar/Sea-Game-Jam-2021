677526b3fa3b7140f4126c6912c6f2aa89761ad1
GavinChin1 <90906013+GavinChin1@users.noreply.github.com>
2021-09-18 01:00:15 +0800

    Partial Player listener script
    Added Death Listener for player (testing code)

--- Assets/JOHN/Scripts/BasePlayer.cs
+++ Assets/JOHN/Scripts/BasePlayer.cs
@@ -8,5 +8,81 @@ using DG.Tweening;
 
 public class BasePlayer : MonoBehaviour
 {
+    public float playerHealth = 1;
 
+    public float playerMovement;
+
+    [SerializeField]
+    private Transform JailPosition;
+
+    private System.Action<string> OnDeath = delegate { };
+
+    private void OnEnable()
+    {
+        OnDeath += Testing;
+    }
+    public virtual void Update()
+    {
+        //if(playerHealth < 1)
+        //{
+        //    Death();
+        //}
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            if(this.tag == "Player1")
+            {
+                Death();
+            }
+        }
+    }
+    public virtual void Revive()
+    {
+
+    }
+
+    public virtual void Death()
+    {
+        //StartCoroutine(DeathRoutine());
+
+        //Notify other player
+        List<BasePlayer> undeads = new List<BasePlayer>();
+        BasePlayer[] temp = FindObjectsOfType<BasePlayer>();
+        string tempDeathTag = "";
+         
+        for (int i =0; i < temp.Length; i ++)
+        {
+            if (temp[i] == this)
+                tempDeathTag = temp[i].tag;
+
+            if(temp[i] != this)
+            {
+                undeads.Add(temp[i]);
+            }
+        }
+        foreach(var undead in undeads)
+        {
+            undead.OnDeath(tempDeathTag);
+        }
+    }
+
+    private void teleport(Transform pos)
+    {
+        transform.position = pos.position;
+    }
+
+    private IEnumerator DeathRoutine()
+    {
+        ///DO death routine here, vulnerable? invulnerable? fall down animation?
+        ///
+
+        yield return new WaitForSeconds(5.0f);
+        teleport(JailPosition.transform);
+    }
+
+    private void Testing(string tag)
+    {
+        Debug.Log(tag + "TAG");
+
+    }
 }
--- Assets/JOHN/Scripts/PlayerMovement.cs
+++ Assets/JOHN/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : BasePlayer
 {
     // Start is called before the first frame update
     void Start()
@@ -11,8 +11,13 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
-        
+        base.Update();
+    }
+
+    public override void Death()
+    {
+        base.Death();
     }
 }
