0692e03ec809cfd99724c00c608e57b1602203f0
GavinChin1 <90906013+GavinChin1@users.noreply.github.com>
2021-09-19 18:02:34 +0800

    asd

--- Assets/JOHN/Scripts/CameraMovement.cs
+++ Assets/JOHN/Scripts/CameraMovement.cs
@@ -42,7 +42,7 @@ public class CameraMovement : MonoBehaviour
         if (hasDead == false)
         {
             midPoint = new Vector3(0, camheight, ((players[0].position.z + players[1].position.z) / 2) - offset);
-            var temp = Mathf.Clamp(midPoint.z, -6.73f, 75.24f);
+            var temp = Mathf.Clamp(midPoint.z, -6.73f, 63f);
             cam.transform.position = new Vector3(midPoint.x, midPoint.y, temp);
             screenBound = Camera.main.ScreenToWorldPoint(midPoint);
         }
@@ -51,7 +51,7 @@ public class CameraMovement : MonoBehaviour
             if (UndeadPlayer() != null)
             {
                 var lerp = Vector3.Lerp(new Vector3(0, camheight, cam.transform.position.z), new Vector3(0, camheight, UndeadPlayer().position.z), 0.3f);
-                var temp = Mathf.Clamp(lerp.z, -6.73f, 75.24f);
+                var temp = Mathf.Clamp(lerp.z, -6.73f, 63f);
                 cam.transform.position = new Vector3(lerp.x, lerp.y, lerp.z);
             }
         }
